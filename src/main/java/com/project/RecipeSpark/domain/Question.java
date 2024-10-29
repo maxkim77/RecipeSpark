d4dab98098ea357223ccae3e467a5c7815c32529
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-10-29 21:32:45 +0900

    domain implementation first ver.
    writing a draft

--- src/main/java/com/project/RecipeSpark/domain/Question.java
+++ src/main/java/com/project/RecipeSpark/domain/Question.java
@@ -1,25 +1,44 @@
 package com.project.RecipeSpark.domain;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@Entity
 @Getter
 @Setter
-@Entity
 public class Question {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long userId;
-	@Column(unique=true)
-	private String username;
-	private String password;
-	@Column(unique=true)
-	private String email;
-
+	private Integer questionId;
+	
+	@Column(length=200)
+	private String title;
+	
+	@Column(columnDefinition="TEXT")
+	private String content;
+	
+	private int voteCount;  // 투표 수를 기록하는 필드
+	
+	private LocalDateTime createDate;
+	
+	private LocalDateTime modifyDate;
+	
+	@OneToMany(mappedBy="question",cascade=CascadeType.ALL, orphanRemoval = true)
+	private List<Answer> answerList;
+	
+	@ManyToOne
+	@JoinColumn(name="authorId")
+	private User authorId;
+	
 }
--- src/main/java/com/project/RecipeSpark/domain/QuestionVoter.java
+++ src/main/java/com/project/RecipeSpark/domain/QuestionVoter.java
@@ -1,5 +1,22 @@
 package com.project.RecipeSpark.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class QuestionVoter {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long Id;
+	
+	@ManyToOne
+	@JoinColumn(name="questionId")
+	private Question question;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
 
 }
