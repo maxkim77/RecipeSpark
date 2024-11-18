5fcad33110d4386988ed8860c346913b745d53e2
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-18 23:53:48 +0900

    Revert ":sparkles: recipe crud update"
    This reverts commit 9371f353e28e5fab92d95e9b8918b5975e64bd76.

--- src/main/java/com/project/RecipeSpark/domain/Recipe.java
+++ src/main/java/com/project/RecipeSpark/domain/Recipe.java
@@ -2,7 +2,13 @@ package com.project.RecipeSpark.domain;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,22 +16,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Recipe {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reviewId;
-
-    @Column(length = 255, nullable = false)
-    private String title;
-
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String content;
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long reviewId;
+	@Column(length=255)
+	private String title;
+	@Column(columnDefinition="TEXT")
+	private String content;
+	
     private LocalDateTime createdAt;
 
-    @Column(length = 255)
-    private String imagePath; // 이미지 경로
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
 }
--- src/main/java/com/project/RecipeSpark/form/RecipeForm.java
+++ src/main/java/com/project/RecipeSpark/form/RecipeForm.java
@@ -1,22 +1,17 @@
 package com.project.RecipeSpark.form;
 
-import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RecipeForm {
-    @NotBlank(message = "제목은 필수 입력 사항입니다.")
-    @Size(max = 255, message = "제목은 최대 255자까지 입력할 수 있습니다.")
+
+    @NotEmpty(message = "제목은 필수 항목입니다.")
     private String title;
 
-    @NotBlank(message = "내용은 필수 입력 사항입니다.")
+    @NotEmpty(message = "내용은 필수 항목입니다.")
     private String content;
-
-    private MultipartFile image; // 이미지 파일
 }
