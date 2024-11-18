8ae9ca25f430b49a4f316fea3434553c77f6cd4d
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-18 23:54:50 +0900

    Revert "Revert "Revert "Revert ":sparkles: recipe crud update""""
    This reverts commit 0b9bb4f7d7a04c35cea5cf0488f108562afe6f6b.

--- src/main/java/com/project/RecipeSpark/domain/Recipe.java
+++ src/main/java/com/project/RecipeSpark/domain/Recipe.java
@@ -2,13 +2,7 @@ package com.project.RecipeSpark.domain;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,17 +10,22 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Recipe {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long reviewId;
-	@Column(length=255)
-	private String title;
-	@Column(columnDefinition="TEXT")
-	private String content;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(length = 255, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
     private LocalDateTime createdAt;
 
-	@ManyToOne
-	@JoinColumn(name="userId")
-	private User user;
+    @Column(length = 255)
+    private String imagePath; // 이미지 경로
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
