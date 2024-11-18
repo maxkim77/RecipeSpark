0b9bb4f7d7a04c35cea5cf0488f108562afe6f6b
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-18 23:54:45 +0900

    Revert "Revert "Revert ":sparkles: recipe crud update"""
    This reverts commit e9d53a0cbf4d1e882953fab854ba73e0a3cac1e6.

--- src/main/java/com/project/RecipeSpark/repository/RecipeRepository.java
+++ src/main/java/com/project/RecipeSpark/repository/RecipeRepository.java
@@ -2,10 +2,18 @@ package com.project.RecipeSpark.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.RecipeSpark.domain.Recipe;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByTitleContaining(String title);
+	
+	List<Recipe> findByTitleContaining(String title);
+	
+	List<Recipe> findByUserUserId(Long userId); // 수정된 부분
+	
+	Page<Recipe> findAll(Pageable pageable);
+
 }
