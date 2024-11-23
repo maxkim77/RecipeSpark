c2715a9a2367e95494d4d8dff92a523a3a37ee65
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-23 18:08:53 +0900

    :sparkles: update pagination&search function

--- src/main/java/com/project/RecipeSpark/repository/AIReviewRepository.java
+++ src/main/java/com/project/RecipeSpark/repository/AIReviewRepository.java
@@ -1,13 +1,18 @@
 package com.project.RecipeSpark.repository;
 
-import com.project.RecipeSpark.domain.AIReview;
-import com.project.RecipeSpark.domain.User;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import com.project.RecipeSpark.domain.AIReview;
+import com.project.RecipeSpark.domain.User;
 
 @Repository
 public interface AIReviewRepository extends JpaRepository<AIReview, Long> {
     List<AIReview> findByUser(User user);
+    Page<AIReview> findByUser(User user, Pageable pageable);
+    Page<AIReview> findByUserAndRecipeInputContainingIgnoreCase(User user, String keyword, Pageable pageable);
 }
