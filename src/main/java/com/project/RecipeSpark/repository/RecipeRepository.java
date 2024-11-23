b2ff7af4dafe46dca217325738fccd877e68819b
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-23 13:10:37 +0900

    :sparkles: Recipe Update Function

--- src/main/java/com/project/RecipeSpark/repository/RecipeRepository.java
+++ src/main/java/com/project/RecipeSpark/repository/RecipeRepository.java
@@ -1,11 +1,17 @@
 package com.project.RecipeSpark.repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.RecipeSpark.domain.Recipe;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByTitleContaining(String title);
+
+    Page<Recipe> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String title, String content, org.springframework.data.domain.Pageable pageable);
 }
